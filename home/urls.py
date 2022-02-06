f15e264988a3bde7be3b43fe21c54bc9ee8bf38d
suleiyadanar <sulei@brandeis.edu>
2022-02-06 00:00:59 -0500

    update2

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from home.views import Home
+from home.views import Home, Explore, Contacts, Connector, Forum
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('home/explore', Explore.as_view(), name='explore'),
+    path('home/contacts', Contacts.as_view(), name='contacts'),
+    path('home/connector', Connector.as_view(), name='connector'),
+    path('home/forum', Forum.as_view(), name='forum'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,23 @@ from django.views import View
 class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'home/home.html')
+
+
+class Explore(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/explore.html')
+
+
+class Contacts(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/contactshtml')
+
+
+class Connector(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/connector.html')
+
+
+class Forum(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'social/post_list.html')
